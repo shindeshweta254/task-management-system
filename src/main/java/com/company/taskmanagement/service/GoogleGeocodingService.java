d3781c11cb1ae21154fa8fb6df8a7772fd362f16
shwetashinde <shindeshweta254@gmail.com>
2026-09-07 15:11:37 +0530

    Log Mappls reverse geocoding response

--- src/main/java/com/company/taskmanagement/service/GoogleGeocodingService.java
+++ src/main/java/com/company/taskmanagement/service/GoogleGeocodingService.java
@@ -35,6 +35,8 @@ public class GoogleGeocodingService {
             Map<String, Object> response =
                     restTemplate.getForObject(url, Map.class);
 
+            System.out.println("MAPPLS FULL RESPONSE: " + response);
+
             if (response == null) {
                 return "Current Location";
             }
